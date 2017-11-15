803134731edf717b9f89a32f9e737caa62b8bb46
bbb <178200328@qq.com>
2017-11-15 21:56:38 +0800

    龚洁作业提交，11-14,11-15

--- gj/gj.2017-11-15/udpser.js
+++ gj/gj.2017-11-15/udpser.js
@@ -2,6 +2,5 @@ var udp = require('dgram');
 var server = udp.createSocket('udp4');
 server.on('message', function (msg, rinfo) {
     console.log(`server got:${msg} from ${rinfo.address}:${rinfo.port}`);
-
 });
 server.bind(4333);
