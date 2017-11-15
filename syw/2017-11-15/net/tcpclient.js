f9b486e518fc7a1773648ade9c538e5990263e64
sywlove <2577472346@qq.com>
2017-11-15 21:59:56 +0800

    sywzuoye

---
+++ syw/2017-11-15/net/tcpclient.js
@@ -0,0 +1,22 @@
+const net =require("net");
+const readline=require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const client=new net.Socket();
+client.connect(8080,"localhost",()=>{
+    rl.on("line",(line)=>{
+        client.write(line);
+    })
+})
+client.on("data",(data)=>{
+    console.log(data.toString());
+})
+client.on("end",()=>{
+    process.exit();
+})
+client.on("error",(err)=>{
+    process.exit();
+}) 
