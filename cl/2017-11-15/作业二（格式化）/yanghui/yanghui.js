641c6489f74f5498d6caae1e59e26e8e95fd001b
chenlu <1450965245@qq.com>
2017-11-15 21:45:45 +0800

    11.15作业

--- cl/2017-11-15/作业二（格式化）/yanghui/yanghui.js
+++ cl/2017-11-15/作业二（格式化）/yanghui/yanghui.js
@@ -1,31 +1,32 @@
 module.exports=function yanghui(n){
 	var tArray = new Array();  
-	for(var i=0;i<n;i++){    
+	var str = "";
+	for(var i = 0;i<n;i++){    
 	 
-		tArray[i]=new Array();  
+		tArray[i] = new Array();  
 	 
-	for(var j=0;j<n;j++){  
+	for(var j = 0;j<n;j++){  
 	 
-		tArray[i][0]=1;
-		tArray[i][i]=1;   
+		tArray[i][0] = 1;
+		tArray[i][i] = 1;   
 	 }
 		
 	}
-	for(var i=2;i<n;i++){    
+	for(var i = 2;i<n;i++){    
 	 
-	for(var j=1;j<i;j++){  
+	for(var j = 1;j<i;j++){  
 	 
-		tArray[i][j]=tArray[i-1][j-1]+tArray[i-1][j]; 
+		tArray[i][j] = tArray[i-1][j-1]+tArray[i-1][j]; 
 	 }
 		
 	}
 	
-	for(var i=0;i<n;i++){    
-	 		 
-			console.log(tArray[i]+" ");
+	for(var i = 0;i<n;i++){    
+	 	
+		str = str+tArray[i]+" "+"\n";
  	
 	}
 	
-	
+	return str;
 
 }
