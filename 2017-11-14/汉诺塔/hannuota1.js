f315093b88a86b4b003c1a547bf1074d8f5ed224
panda2451 <767181742@qq.com>
2017-11-15 19:22:35 +0800

    文件夹修改，格式整理，返回数组

---
+++ 2017-11-14/汉诺塔/hannuota1.js
@@ -0,0 +1,11 @@
+const hannuota = require('./hannuota');
+const readline = require('readline');// 引入readline模块
+
+var rl = readline.createInterface({//创建readline接口实例
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("黄金圆盘片数是:", function (n) {// question方法
+    console.log(hannuota(n));
+})
