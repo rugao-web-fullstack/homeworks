c31b0f2962e23d2ac24e6e19676a8b8f8b896bd6
panda2451 <767181742@qq.com>
2017-11-15 20:49:37 +0800

    关于，后面有内容加上空格的修改

--- 2017-11-14/汉诺塔/hanoi.js
+++ 2017-11-14/汉诺塔/hanoi.js
@@ -4,20 +4,20 @@ module.exports = function (num) {
     var b = 'b';
     var c = 'c';
 
-    Han(a,b,c,num);
+    Han(a, b, c, num);
 
-    function Han(a,b,c,n){
+    function Han(a, b, c, n){
         if(n == 1){
-            Move(a,b);
+            Move(a, b);
         }else{
-            Han(a,c,b,n-1);
-            Move(a,b);
-            Han(c,b,a,n-1);
+            Han(a, c, b, n-1);
+            Move(a, b);
+            Han(c, b, a, n-1);
         }
     }
 
-    function Move(num1,num2){
-        arr.push('从盘'+num1+'移动一个到盘'+num2);
+    function Move(num1, num2){
+        arr.push('从盘' + num1 + '移动一个到盘' + num2);
     }
 
     return arr;
--- 2017-11-14/汉诺塔/hanoi1.js
+++ 2017-11-14/汉诺塔/hanoi1.js
@@ -6,7 +6,7 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('请输入数列长度：',function(n){
+rl.question('请输入数列长度：', function(n){
     var arr = Hanoi(n);
     for(var i = 0; i < arr.length ; i++){
         console.log(arr[i]);
