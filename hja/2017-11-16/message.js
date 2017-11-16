834433d3bc4ff1933e32ce5a8277b31e8dab2955
Snow <1011341509@qq.com>
2017-11-16 20:24:57 +0800

    作业

---
+++ hja/2017-11-16/message.js
@@ -0,0 +1,20 @@
+function Message(event, arr) {
+
+    this.event = event;
+    console.log('创建message对象');
+    //事件监听
+    this.event.on('register', (user) = > {
+        console.log('register事件触发');
+    var x = this.email(user);
+    arr.splice(0, 1);
+    arr.push(x);
+})
+    ;
+}
+
+Message.prototype.email = function (user) {
+    var msg = ('用户名:' + user.username + ' 密码:' + user.password + ' 邮箱:' + user.email);
+    return msg;
+
+};
+exports.Message = Message;
