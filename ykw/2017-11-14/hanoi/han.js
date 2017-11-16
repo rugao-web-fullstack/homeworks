c051b6f4ec90e25bbee1bd7a85fe85fd370daefc
rujiangfanfanzi <1187256832@qq.com>
2017-11-16 11:22:22 +0800

    ykw

---
+++ ykw/2017-11-14/hanoi/han.js
@@ -0,0 +1,10 @@
+module.exports = function hanoi(n,x,y,z) {
+    if(n==1){
+        console.log("Move "+n+" from "+x+" to "+z);
+    }
+    else{
+        hanoi(n-1,x,z,y);
+        console.log("Move "+n+" from "+x+" to "+z)
+        hanoi(n-1,y,x,z);
+    }
+}
