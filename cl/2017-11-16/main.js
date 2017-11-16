a17e5370e1be0f01b9ab770b769ec5f3ba692985
chenlu <1450965245@qq.com>
2017-11-16 21:35:42 +0800

    11.16作业

---
+++ cl/2017-11-16/main.js
@@ -0,0 +1,8 @@
+﻿const User = require("./user").User;
+const Message = require("./message").Message;
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+const user = new User(emitter);
+const message = new Message(emitter);
+
+user.register("陈三", "oo", "wwww@qq.com");
---
+++ cl/2017-11-16/message.js
@@ -0,0 +1,13 @@
+function Message(event) {
+	this.event = event;
+	console.log("message:constructor");
+	this.event.on("user-register", (user) => {
+	console.log("message: on user-register");
+		this.mail(user);
+	});
+}
+
+Message.prototype.mail = function(user) {
+	console.log("email is sent to <" + user.username + ">" + user.email);
+};
+exports.Message = Message;
