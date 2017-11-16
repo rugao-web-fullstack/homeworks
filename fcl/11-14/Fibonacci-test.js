dfe01b8287483d72181f04e13d15838df0cdf4a1
O-Mr-Wei-O <1223157723@qq.com>
2017-11-16 10:48:22 +0800

    范成磊

--- fcl/11-14/Fibonacci-test.js
+++
@@ -1,9 +0,0 @@
-const Fibtest = require("./module/Fibonacci.js");
-const readline = require('readline'); // 引入readline模块
-var rl = readline.createInterface({ //创建readline接口实例
-	input: process.stdin,
-	output: process.stdout
-});
-rl.question("请输入你想要的数字:", function(n) { // question方法
-	console.log(Fibtest(n));
-})
--- fcl/11-14/hanoi-test.js
+++
@@ -1,4 +0,0 @@
-const hanoi = require("./module/hanoi");
-let num = process.argv[2] || 5;
-let move = hanoi(num, "A", "B", "C");
-console.log(num + "的汉诺塔顺序为：\n" + move);
