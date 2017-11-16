8c9ac1a59c5fe40f6e11481fcb04fdb2cc743d18
wang <1447516660@qq.com>
2017-11-17 00:16:20 +0800

    二次提交，已使用事件模型

--- wd/2017-11-16/上课文件/message.js
+++ wd/2017-11-16/上课文件/message.js
@@ -1,13 +1,11 @@
 function Message(event) {
 	this.event = event;
-	console.log("message:constructor");
 	this.event.on("user-register", (user) => {
-		console.log("message: on user-register");
 		this.mail(user);
 	});
 }
 
 Message.prototype.mail = function(user) {
-	console.log("email is sent to <" + user.username + ">" + user.email);
+	console.log("register success! username is " + user.username);
 };
 exports.Message = Message;
--- wd/2017-11-16/上课文件/user.js
+++ wd/2017-11-16/上课文件/user.js
@@ -1,12 +1,9 @@
 function User(event) {
 	this.event = event;
-	console.log("user: constructor");
 }
-User.prototype.register = function(username, password, email) {
-	console.log("user: register");
+User.prototype.register = function(username, password) {
 	this.username = username;
 	this.password = password;
-	this.email = email;
 	this.event.emit("user-register", this);
 };
 exports.User = User;
