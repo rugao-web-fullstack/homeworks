f52eb5bd725d238d1e46d703b4499bb289a03c94
DingYue0724 <dingyue0724@gmail.com>
2017-11-16 11:15:12 +0800

    作业上传

---
+++ homeworks/dy/2017-11-15/tcp-client.js
@@ -0,0 +1,27 @@
+var net = require("net");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var client = new net.Socket();
+client.connect(8080, 'localhost', function () {
+	console.log('connect to the tcp server!');
+	console.log("if you want to stop,please input:");
+	console.log("0: stop process; 1: close server");
+
+	rl.question('please input: ', (answer) => {
+		if (answer == "0") {
+			client.write("0");
+		} else if (answer == "1") {
+			client.write("1");
+		}
+		console.log(answer);
+ 		rl.close();
+	});
+});
+client.on("data", function (data) {
+	console.log("close server");
+});
