bbfba452f1dc192c155317a5774166dc7ea82264
PandaMino <1372438884@qq.com>
2017-11-16 10:46:47 +0800

    卢丹格式修改1

--- ld/2017-11-14/Modules/Hanoi.js
+++ ld/2017-11-14/Modules/Hanoi.js
@@ -1,10 +1,10 @@
-module.exports = function Hanoi(n,a,b,c) {
-	
-	if(n==1) {
-		console.log("Move "+n+" from "+a+" to "+c);
-	}else {
-		Hanoi(n-1,a,c,b);
-		console.log("Move "+n+" from "+a+" to "+c);
-		Hanoi(n-1,b,a,c);
+module.exports = function Hanoi(n, a, b, c) {
+
+	if (n == 1) {
+		console.log("Move " + n + " from " + a + " to " + c);
+	} else {
+		Hanoi(n - 1, a, c, b);
+		console.log("Move " + n + " from " + a + " to " + c);
+		Hanoi(n - 1, b, a, c);
 	}
 }
