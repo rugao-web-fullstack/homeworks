ffdf1c003f95029c8c6394f312dc5142fe862849
Amaya-wyw <18360939510@163.com>
2017-11-17 16:23:17 +0800

    2017-11-16作业第二次提交

--- wyw/2017-11-16/message.js
+++ wyw/2017-11-16/message.js
@@ -1,13 +1,23 @@
-function Message(event) {
-	this.event = event;
-	console.log("message:constructor");
-	this.event.on("user-register", (user) => {//监听器，事件触发则执行监听函数
-	console.log("message: on user-register");
-		this.mail(user);
-	});
+function Message(event, sockts) {
+    this.event = event;
+    this.sockets = sockts;
+    console.log("message:constructor");
+    let self = this;
+    this.event.on("user-register", function (socket, user) {
+        console.log("message: on user-register");
+        self.onUserRegister(socket, user);
+    });
 }
 
-Message.prototype.mail = function(user) {
-	console.log("email is sent to <" + user.username + ">" + user.email);
+Message.prototype.onUserRegister = function (socket, user) {
+    for (var i = 0; i < this.sockets.length; i++) {
+        let s = this.sockets[i];
+        if (s != socket) {
+            s.write("user" + user.username + ">registered\n");
+            console.log("user<" + user.username + ">registered\n");
+        } else {
+            s.write("You've registered successful !\n");
+        }
+    }
 };
 exports.Message = Message;
--- wyw/2017-11-16/user.js
+++ wyw/2017-11-16/user.js
@@ -1,13 +1,12 @@
 function User(event) {
-	this.event = event;
-	console.log("user: constructor");
+    this.event = event;
+    console.log("User: constructor");
 }
-User.prototype.register = function(username, password, email) {
-	console.log("user: register");
-	this.username = username;
-	this.password = password;
-	this.email = email;
-	this.event.emit("user-register", this);
+
+User.prototype.register = function (username, password, email) {
+    console.log("User: register");
+    this.username = username;
+    this.password = password;
+    this.email = email;
 };
 exports.User = User;
-
