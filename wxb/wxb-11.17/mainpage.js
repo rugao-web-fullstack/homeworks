7eb6732f32746475065c5ebeaf2d2dae44220828
O-Mr-Wei-O <1223157723@qq.com>
2017-11-20 21:55:43 +0800

    11.20作业提交
    还有读取功能没写

---
+++ wxb/wxb-11.17/mainpage.js
@@ -0,0 +1,21 @@
+function mainpage(socket) {
+    socket.on("mainpage", function () {
+        socket.write("请选择功能:\n 1.注册 2.登录\n");
+        socket.on("data", function d(data) {
+            //-------------不加这行会有不同输出
+            data = data.toString().replace(/[^a-zA-Z0-9_ ]?/g, "");
+            if (data == '1') {
+                socket.removeListener("data",d);
+                socket.emit("register");
+                // console.log(1);
+            } else if (data == '2') {
+                socket.removeListener("data",d);
+                socket.emit("login");
+                // console.log(2);
+            }
+        });
+    });
+
+}
+
+module.exports = mainpage;
---
+++ wxb/wxb-11.17/sendmail.js
@@ -0,0 +1,20 @@
+var use_info = require("./user_info").user_info;
+
+function sendmail(socket) {
+    socket.on("sendmail",function () {
+        socket.write("\n请输入邮箱，标题，内容\n");
+        socket.on("data",function d(data) {
+            //---去除空格
+            data = data.toString().replace(/(^\s*)|(\s*$)/g, "");
+            var user = data.split(",")[0];
+            var title = data.split(",")[1];
+            var content = data.split(",")[2];
+            var index=use_info.returnmailArr().indexOf(user);
+            // use_info.mailTitle(user, title);
+            // use_info.mailContent(user, content);
+            use_info.returnSockets()[index].write(user+"，您有一封信邮件，请查看（如果没有登录请先登录）\n");
+        });
+    });
+}
+
+module.exports= sendmail;
