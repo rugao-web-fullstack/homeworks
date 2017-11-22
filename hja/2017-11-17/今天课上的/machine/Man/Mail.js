b3d7cc3cbfc596f2755df55adf96d6529ee064f1
Snow <1011341509@qq.com>
2017-11-22 20:32:48 +0800

    作业

--- hja/2017-11-17/今天课上的/machine/Man/Mail.js
+++ hja/2017-11-17/今天课上的/machine/Man/Mail.js
@@ -1,5 +1,29 @@
 const User = require('./user').User;
-let mails = {};
+const fs = require('fs');
+const filename = '../data/mail.json';
+
+// let mails = {};
+Mail.readJSON = function (filename,callback) {
+        let data = [];
+        const rs = fs.createReadStream(filename);
+        rs.on('data',function (chunk) {
+            data.push(chunk);
+        });
+        rs.on('end',function () {
+            let json = String(Buffer.concat(data,data.length));
+            callback(json);
+        });
+};
+
+Mail.writeJSON = function (filename,json,callback) {
+    const ws = fs.createWriteStream(filename);
+    ws.on(JSON.stringify(json));
+    ws.end();
+    ws.on('finish',function () {
+        callback(false);
+    });
+
+};
 
 function Mail(sender, receiver, title, body) {
     this.sender = sender;
@@ -9,6 +33,7 @@ function Mail(sender, receiver, title, body) {
 }
 
 Mail.send = function (sender, receiver, title, body) {
+    let mails = this.readJSON(filename,);
     if (!mails[receiver]) {
         mails[receiver] = [];
     }
