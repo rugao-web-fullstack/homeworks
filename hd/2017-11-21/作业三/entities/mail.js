05c20985cbd56878116b3da3efbde2e4c329e490
haungda <353656864@qq.com>
2017-11-22 19:39:22 +0800

    黄达21号作业提交

---
+++ hd/2017-11-21/作业三/entities/mail.js
@@ -0,0 +1,66 @@
+let states = require("../states").states;
+const path = require("path");
+const UserManager = require('./user').User;
+
+
+// 创建Storage，也就是加载或者保存的对象
+const FILENAME = "../data/mail.json";
+
+const Storage = require("./storage").Storage;
+const storage = new Storage(
+    path.resolve(
+        path.dirname(__filename), FILENAME));
+
+function Mail(sender, receiver, title, body) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.title = title;
+    this.body = body;
+}
+
+Mail.send = function (sender, receiver, title, body, cb) {
+    storage.read((error, mails) => {
+        if (error) {
+            console.log(error.stack);
+            cb(error);
+            return;
+        }
+
+        if (!mails) {
+            mails = {};
+        }
+
+        if (!mails[receiver]) {
+            mails[receiver] = [];
+        }
+        let mail = new Mail(sender, receiver, title, body);
+
+        mails[receiver].push({
+            read: false,
+            mail: mail
+        });
+
+        storage.save(mails, (error) => {
+            if (error) {
+                cb(error);
+                return;
+            }
+            let receiverSocket = UserManager.getSocket(receiver)
+            receiverSocket.emit(states.MAIL_NEW, sender, mail);
+            cb(false);
+        })
+    });
+};
+
+Mail.get = function (user, cb) {
+    storage.read((error, mails) => {
+        if (error) {
+            cb(error);
+            return;
+        }
+        return cb(false, mails[user]);
+    });
+
+};
+
+exports.Mail = Mail;
