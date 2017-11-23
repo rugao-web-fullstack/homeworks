2e4548a2423b3c8c6531e2849be7af26e357cc48
Eastwood <31194686+Eastwood95@users.noreply.github.com>
2017-11-23 14:51:09 +0800

    相对地址修改

--- cd/2017-11-21/作业2/dir.js
+++ cd/2017-11-21/作业2/dir.js
@@ -1,7 +1,7 @@
 var fs = require('fs');  
 var path = require('path');  
   
-var filePath = path.resolve('/home/eastwood');  
+var filePath = path.resolve('..');  
   
 fileDisplay(filePath);  
   
