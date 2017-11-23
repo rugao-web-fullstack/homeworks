a5571c5733eb82bd6f2792f816d762372b23a51b
pan <770258173@qq.com>
2017-11-23 23:12:53 +0800

    添加目录名

--- pqq/2017-11-21/zuoye2/2.js
+++ pqq/2017-11-21/zuoye2/2.js
@@ -8,6 +8,7 @@ var  rl = readline.createInterface({
 });
 rl.question("请输入需要遍历的文件夹绝对路径(例:/Users/pan/Documents)：", function(filepath) {
 	//调用文件遍历方法  
+	console.log('\n------------打印'+filepath+'下的文件----------\n');
 	fileDisplay(filepath); 
     	rl.close();
 });
--- pqq/2017-11-21/zuoye2/modules/file.js
+++ pqq/2017-11-21/zuoye2/modules/file.js
@@ -11,16 +11,18 @@ function file (filePath) {
 				//获取当前文件的绝对路径  
 				var filedir = path.join(filePath, filename);  
 				//根据文件路径获取文件信息，返回一个fs.Stats对象  
-				fs.stat(filedir, function (eror, stats) {  
+				fs.stat(filedir, function (eror, stats) { 
 					if (eror) {  
 						console.warn('获取文件stats失败');  
 					} else {  
 						var isFile = stats.isFile();//是文件  
-						var isDir = stats.isDirectory();//是文件夹  
+						var isDir = stats.isDirectory();//是文件夹 
+						
 						if(isFile){  
-							console.log(filedir);  
+							console.log('--文件：'+filedir);  
 						}  
 						if(isDir){  
+							console.log('------目录名：'+ filedir+'~~~~~~~~~~\n');
 							file(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件  
 						}  
 					}  
