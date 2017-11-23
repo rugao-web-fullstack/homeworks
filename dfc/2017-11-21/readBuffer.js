97ecf68078f6556f09f694692e9a1297dba0bab0
unknown <309425239@qq.com>
2017-11-23 16:43:24 +0800

    党方超2017-11-21作业---buffer

--- dfc/2017-11-21/readBuffer.js
+++ dfc/2017-11-21/readBuffer.js
@@ -1,14 +1,20 @@
 const fs = require("fs");
-const rs = fs.createReadStream('buffer.txt');
-let b;
-let bb;
-rs.on('readable', () => {
-    b = rs.read();
-    if(b){
-        bb = b.readInt32BE();
-        console.log(bb);
+const rs = fs.createReadStream("demo.txt");
+
+var readBuff = [];
+
+rs.on("readable", function () {
+    var data = rs.read();
+    if (data) {
+        readBuff.push(data);
     }
 });
-rs.on('end', () => {
-    console.log('on readable end');
+rs.on("end", function () {
+    readBuff = Buffer.concat(readBuff);
+    console.log("UInt32 = " + readBuff.readUInt32BE(0).toString(16));
+    console.log("Int32 = " + readBuff.readInt32BE(4));
+    console.log("UInt8 = " + readBuff.readUInt8(8));
+    console.log("Int8 = " + readBuff.readInt8(9));
+    console.log("Float = " + readBuff.readFloatBE(10));
+    console.log("Double = " + readBuff.readDoubleBE(14));
 });
--- dfc/2017-11-21/writeBuffer.js
+++ dfc/2017-11-21/writeBuffer.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
-const ws = fs.createWriteStream('buffer.txt');
-let d = Buffer.alloc(4);
-d.writeInt32BE(1262682622,0);
-ws.write(d);
-console.log(d);
-ws.end('');
-ws.on('finish', () => {
-    console.log('Writable finished');
+const ws = fs.createWriteStream("demo.txt");
+
+var writeBuff = Buffer.alloc(22);
+writeBuff.writeUInt32BE(0xfeedface, 0);
+writeBuff.writeInt32BE(-10, 4);
+writeBuff.writeUInt8(0x23, 8);
+writeBuff.writeInt8(-10, 9);
+writeBuff.writeFloatBE(1.23, 10);
+writeBuff.writeDoubleBE(-1.23, 14);
+
+ws.end(writeBuff, function () {
+    console.log("导入成功");
 });
