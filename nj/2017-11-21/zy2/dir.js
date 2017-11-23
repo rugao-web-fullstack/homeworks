620324521afce95b08a753b8b54c1b4fc93b5b8a
nj0616 <875221695@qq.com>
2017-11-23 20:39:52 +0800

    作业提交

---
+++ nj/2017-11-21/zy2/dir.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.resolve('..');
+
+function fileDisplay(filePath) {
+    fs.readdir(filePath, function (err, files) {
+        if (err) {
+            console.warn(err);
+        } else {
+            files.forEach(function (filename) {
+                var filedir = path.join(filePath, filename);
+                fs.stat(filedir, function (eror, stats) {
+                    if (eror) {
+                        console.log('获取文件失败');
+                    } else {
+                        var isFile = stats.isFile();//是文件  
+                        var isDir = stats.isDirectory();//是文件夹  
+                        if (isFile) {
+                            console.log(filedir);
+                        }
+                        if (isDir) {
+                            fileDisplay(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件  
+                        }
+                    }
+                })
+            });
+        }
+    });
+}
+
+fileDisplay(filePath);
