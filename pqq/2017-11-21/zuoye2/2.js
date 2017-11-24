ad34b85bfb0efe5906f1561915d6d617eda68901
pan <770258173@qq.com>
2017-11-24 08:43:44 +0800

    提交

--- pqq/2017-11-21/zuoye2/2.js
+++ pqq/2017-11-21/zuoye2/2.js
@@ -1,14 +1,14 @@
 var readline = require('readline');
 const fileDisplay = require("./modules/file").file;
 //解析需要遍历的文件夹  
-var filePath;  
-var  rl = readline.createInterface({
+var filePath;
+var rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-rl.question("请输入需要遍历的文件夹绝对路径(例:/Users/pan/Documents)：", function(filepath) {
+rl.question("请输入需要遍历的文件夹绝对路径(例:/Users/pan/Documents)：", function (filepath) {
 	//调用文件遍历方法  
-	console.log('\n------------打印'+filepath+'下的文件----------\n');
-	fileDisplay(filepath); 
-    	rl.close();
+	console.log('\n------------打印' + filepath + '下的文件----------\n');
+	fileDisplay(filepath);
+	rl.close();
 });
