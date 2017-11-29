59c49066e943e1cebbc72545e5defc00c6b4e34d
ErinGQ <ErinGQ@163.com>
2017-11-29 08:57:41 +0800

    修改2017-11-27作业提交

--- gq/2017-11-27/session-cookie.js
+++ gq/2017-11-27/session-cookie.js
@@ -1,47 +1,30 @@
-var http = require(http);
-var uuid = require(uuid / v4);
+var http = require('http');
+var uuid = require('uuid/v4');
+var url = require("url");
+var qs = require("querystring");
 var session = {};
 
 http.createServer(function (req, res) {
-    var cookie = req.headers[cookie];
-    var user;
-    var insex = -1;
-    var sid;
+	var user;
+	var sid;
+	var parsedUrl = url.parse(req.url);
+	var query = qs.parse(parsedUrl.query);
 
-    if (cookie) {
-        var cookies = cookie.split(';');
-        console.log(cookie);
-        console.log(cookies);
-        if (cookies) {
-            for (var i = 0; i < cookies.length; i++) {
-                if (cookies[i].indexOf('sid') === 0) {
-                    sid = cookies[i].split('='[1]);
-                    console.log(sid);
-                    index = i;
-                    break;
-                }
-            }
-            console.log('index =' + index);
-            if (index !== -1) {
-                user = session[sid];
-            }
-        }
-    }
-    if (!user) {
-        var user = {
-            id: uuid(),
-            username: "user-" + new Date().getTime(),
-            password: 'password'
-        };
-
-        var sid = uuid();
-        session[sid] = user;
-        res.writeHead(200, {
-            'Set-Cookie': "sid" + sid,
-            'Content-Type': 'text/plain'
-        });
-    }
-    res.write('you name is ' + user.username);
-    res.write('Hello Word!');
-    res.end();
+	if (query.sid) {
+		user = session[query.sid];
+	}
+	if (!user) {
+		user = {
+			id: uuid(),
+			username: "user" + new Date().getTime(),
+			password: "password"
+		}
+		var sid = uuid();
+		session[sid] = user;
+		res.writeHead(301, {
+			"Location": "/?sid=" + sid,
+		});
+	}
+	res.write("hello:" + user.username);
+	res.end();
 }).listen(8080);
