797c35b210ccb6858dde82a6e985bf0d9d2e97e3
Eastwood <31194686+Eastwood95@users.noreply.github.com>
2017-11-30 21:54:56 +0800

    数据库修改

--- cd/2017-11-30/email.sql
+++ cd/2017-11-30/email.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 30/11/2017 21:40:18
+ Date: 30/11/2017 21:53:47
 */
 
 SET NAMES utf8mb4;
@@ -23,6 +23,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `email`;
 CREATE TABLE `email`  (
   `eid` int(10) NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `sender` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `receiver` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -30,8 +31,11 @@ CREATE TABLE `email`  (
   PRIMARY KEY (`eid`) USING BTREE,
   INDEX `sender`(`sender`) USING BTREE,
   INDEX `receiver`(`receiver`) USING BTREE,
+  INDEX `email`(`email`) USING BTREE,
+  INDEX `email_address`(`email`) USING BTREE,
   CONSTRAINT `email_receive` FOREIGN KEY (`receiver`) REFERENCES `user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `emial_send` FOREIGN KEY (`sender`) REFERENCES `user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT `emial_send` FOREIGN KEY (`sender`) REFERENCES `user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `email_address` FOREIGN KEY (`email`) REFERENCES `user` (`email`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -45,6 +49,7 @@ CREATE TABLE `user`  (
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user_send`(`username`) USING BTREE,
+  INDEX `email`(`email`) USING BTREE,
   CONSTRAINT `user_receive` FOREIGN KEY (`username`) REFERENCES `email` (`receiver`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `user_send` FOREIGN KEY (`username`) REFERENCES `email` (`sender`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
