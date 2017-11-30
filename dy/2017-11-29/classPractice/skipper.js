64af77822f2c08137e6f3a24760978817d829aed
DingYue0724 <dingyue0724@gmail.com>
2017-11-30 21:55:46 +0800

    2017-11-29和2017-11-30课上代码提交

---
+++ dy/2017-11-29/classPractice/skipper.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var express = require('express');
+var skipper = require('skipper');
+var nunjucks = require('nunjucks');
+
+var app = express();
+app.use(skipper());
+
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+});
+
+// upload
+app.post('/', function (req, res) {
+    req.file('avatar').upload(function (err, uploadFiles) {
+        if (err) return res.send(500, err);
+        return res.json ({
+            message: uploadFiles.length + 'file upload successfully!',
+            files: uploadFiles
+        });
+    });
+});
+
+app.get('/', function (req, res) {
+    req.render('file.html');
+});
+app.listen(3000);
