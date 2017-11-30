fa14276783ad32bb0578c0af53e96a912a534dd6
Amaya-wyw <18360939510@163.com>
2017-11-30 22:02:07 +0800

    29号作业

---
+++ wyw/2017-11-29/b/nunjucks.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var nunjucks = require('nunjucks');
+var app = express();
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+});
+app.get('/', function (req, res) {
+    res.render('main.html', { name: "Greate" });
+});
+app.listen(8080);
