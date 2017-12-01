fa23b0e8dd0581b0dc7b2b4365f8f0fcd3bba996
unknown <309425239@qq.com>
2017-12-01 15:04:21 +0800

    党方超2017-11-30作业

--- dfc/2017-11-29/dmail/routes/users.js
+++ dfc/2017-11-29/dmail/routes/users.js
@@ -7,6 +7,17 @@ router.get('/login', function(req, res, next) {
 		title: '登录'
 	});
 });
+
+router.post('/login', function(req, res, next) {
+	let userMessage = req.body;
+	if(userMessage.username === 'root'){
+		if(userMessage.password === '123'){
+			res.redirect('/main');
+		}
+	}else{
+		res.write('404');
+	}
+});
 router.get('/register', function(req, res, next) {
 	res.render('register', {
 		title: '注册'
