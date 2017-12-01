d875bed0661444abfdcf52499945743aee5d253f
chenlu <1450965245@qq.com>
2017-12-01 11:51:42 +0800

    数据表修改

--- cl/2017-11-30/mail.sql
+++ cl/2017-11-30/mail.sql
@@ -10,16 +10,16 @@ Target Server Type    : MYSQL
 Target Server Version : 50520
 File Encoding         : 65001
 
-Date: 2017-11-30 21:17:49
+Date: 2017-12-01 11:44:56
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for email
+-- Table structure for mail
 -- ----------------------------
-DROP TABLE IF EXISTS `email`;
-CREATE TABLE `email` (
+DROP TABLE IF EXISTS `mail`;
+CREATE TABLE `mail` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `content` varchar(255) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE `email` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of email
+-- Records of mail
 -- ----------------------------
 
 -- ----------------------------
@@ -39,12 +39,27 @@ DROP TABLE IF EXISTS `mailbox`;
 CREATE TABLE `mailbox` (
   ` id` int(11) NOT NULL AUTO_INCREMENT,
   `user` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
+  `mailbox` varchar(255) NOT NULL,
   PRIMARY KEY (` id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of mailbox
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mail_mailbox
+-- ----------------------------
+DROP TABLE IF EXISTS `mail_mailbox`;
+CREATE TABLE `mail_mailbox` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `mail` varchar(255) NOT NULL,
+  `mailbox` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of mail_mailbox
 -- ----------------------------
 
 -- ----------------------------
