c4e501838d21c947929f0f8e589c2247aa296b0b
nylovelan <nylovelan@outlook.com>
2017-12-01 15:56:52 +0800

    mail用户注册

--- ny/2017-11-29/mail/main.js
+++ ny/2017-11-29/mail/main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var nunjucks = require('nunjucks');
 var bodyParser  = require("body-parser"); 
 var login = require("./login");
+var register = require("./register");
 var app = express();
 
 nunjucks.configure('html', {
@@ -10,6 +11,7 @@ nunjucks.configure('html', {
 });
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.post('/login', login);
+app.post('/register', register);
 
 app.get('/', function (req, res) {
 	res.render("index.html");
