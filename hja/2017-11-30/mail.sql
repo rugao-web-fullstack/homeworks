e57f91e50a7771034260635186ccf5c94d7d60db
Snow <1011341509@qq.com>
2017-12-01 10:30:14 +0800

    作业

--- hja/2017-11-30/mail.sql
+++ hja/2017-11-30/mail.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2017-12-01 10:11:58
+Date: 2017-12-01 10:29:01
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,9 +25,9 @@ CREATE TABLE `mail` (
   `sender` varchar(64) NOT NULL,
   `title` varchar(64) NOT NULL,
   `content` varchar(1000) NOT NULL,
-  `readState` varchar(12) NOT NULL,
+  `is_read` tinyint(12) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of mail
@@ -39,7 +39,7 @@ CREATE TABLE `mail` (
 DROP TABLE IF EXISTS `mailbox`;
 CREATE TABLE `mailbox` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `address` int(128) NOT NULL,
+  `user` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -53,8 +53,8 @@ CREATE TABLE `mailbox` (
 DROP TABLE IF EXISTS `mail_emailbox`;
 CREATE TABLE `mail_emailbox` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `address` varchar(64) NOT NULL,
-  `mail` varchar(64) NOT NULL,
+  `mailbox` int(64) NOT NULL,
+  `mail` int(64) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -71,23 +71,8 @@ CREATE TABLE `user` (
   `username` varchar(32) NOT NULL,
   `password` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
--- ----------------------------
-
--- ----------------------------
--- Table structure for user_emialbox
--- ----------------------------
-DROP TABLE IF EXISTS `user_emialbox`;
-CREATE TABLE `user_emialbox` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `address` int(11) NOT NULL,
-  `user` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user_emialbox
 -- ----------------------------
