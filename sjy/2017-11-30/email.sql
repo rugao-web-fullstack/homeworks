d46979fcac9444c21b9f11aafed85e85afe733bc
Songjieyu <sjyjsnt@163.com>
2017-12-01 14:41:29 +0800

    mysqldump导出

--- sjy/2017-11-30/email.sql
+++ sjy/2017-11-30/email.sql
@@ -1,78 +1,103 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2017-11-30 22:39:02
+-- 服务器版本： 5.7.20-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
-Source Server         : 本地连接
-Source Server Version : 50520
-Source Host           : localhost:3306
-Source Database       : emial
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50520
-File Encoding         : 65001
 
-Date: 2017-12-01 13:19:49
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `email`
+--
 
--- ----------------------------
--- Table structure for mail
--- ----------------------------
-DROP TABLE IF EXISTS `mail`;
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `mail`
+--
+
 CREATE TABLE `mail` (
   `id` int(11) NOT NULL,
   `theme` varchar(255) NOT NULL,
   `body` varchar(255) NOT NULL,
   `receiver` varchar(255) NOT NULL,
-  `sender` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `sender` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of mail
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for mailbox
--- ----------------------------
-DROP TABLE IF EXISTS `mailbox`;
+--
+-- 表的结构 `mailbox`
+--
+
 CREATE TABLE `mailbox` (
   `id` int(255) NOT NULL,
-  `user` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `user` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of mailbox
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for mailbox_mail
--- ----------------------------
-DROP TABLE IF EXISTS `mailbox_mail`;
+--
+-- 表的结构 `mailbox_mail`
+--
+
 CREATE TABLE `mailbox_mail` (
   `id` int(255) NOT NULL,
   `mail` int(255) NOT NULL,
-  `mailbox` int(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `mailbox` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of mailbox_mail
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
+--
+-- 表的结构 `user`
+--
+
 CREATE TABLE `user` (
   `id` int(255) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `address` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of user
--- ----------------------------
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mail`
+--
+ALTER TABLE `mail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mailbox`
+--
+ALTER TABLE `mailbox`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mailbox_mail`
+--
+ALTER TABLE `mailbox_mail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
