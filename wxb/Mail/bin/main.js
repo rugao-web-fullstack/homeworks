f19e093839253a27bcd0e325c5fb4752a55581a1
O-Mr-Wei-O <1223157723@qq.com>
2017-12-02 10:59:08 +0800

    邮件系统

---
+++ wxb/Mail/bin/main.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var main = require("../routes/main");
+
+
+//---设置模板
+app.set("view engine", "jade");
+
+
+app.use("/", main);
+app.use("/register", main);
+app.use("/login", main);
+app.use("/welcome",main);
+app.use("/writeMail",main);
+app.use("/checkMail",main);
+app.use("/checkMail_all",main);
+
+
+app.listen(3000);
