0653b3cf8075cd49d147ccbf39941c98c2a28412
chenlu <1450965245@qq.com>
2017-12-02 13:36:40 +0800

    已完成部分格式化

--- cl/2017-12-01/mail/index.js
+++ cl/2017-12-01/mail/index.js
@@ -45,33 +45,33 @@ app.post('/main', urlencodedParser, function (req, res) {
 app.get('/write', function (req, res) {
     res.sendFile(__dirname + "/public/" + "write.html");
 })
-app.get('/read', urlencodedParser,function (req, res) {
+app.get('/read', urlencodedParser, function (req, res) {
     basic(function (con) {
-        var id1=1;
-        var address1="11";
-        var title1="11";
-        var content1="11";
-        var sender1="11";
-        var sql="select id from user where username = '"+currentUser+"'";
+        var id1 = 1;
+        var address1 = "11";
+        var title1 = "11";
+        var content1 = "11";
+        var sender1 = "11";
+        var sql = "select id from user where username = '" + currentUser + "'";
         con.query(sql, function (err, result) {
             if (err) throw err;
             result.forEach(function (item) {
-                id1=item.id;
+                id1 = item.id;
             });
-            var sql2="select address from mailbox where user ='"+id1+"'";
+            var sql2 = "select address from mailbox where user ='" + id1 + "'";
             con.query(sql2, function (err, result) {
                 if (err) throw err;
                 result.forEach(function (item) {
-                    address1=item.address;
+                    address1 = item.address;
                 });
-                var sql3="select * from mail where receiver ='"+address1+"'";
+                var sql3 = "select * from mail where receiver ='" + address1 + "'";
                 con.query(sql3, function (err, result) {
                     if (err) throw err;
                     console.log(result);
                     result.forEach(function (item) {
-                       title1=item.title;
-                       sender1=item.sender;
-                       content1=item.content;
+                        title1 = item.title;
+                        sender1 = item.sender;
+                        content1 = item.content;
                     });
                 });
             });
@@ -84,10 +84,10 @@ app.post('/login', urlencodedParser, function (req, res) {
         var username = req.body.username;
         var password = req.body.password;
         var address = req.body.address;
-        var sql2="select * from user where username ='"+username+"'";
+        var sql2 = "select * from user where username ='" + username + "'";
         con.query(sql2, function (err, result) {
             if (err) throw err;
-            if(result.length === 0){
+            if (result.length === 0) {
                 var sql = "insert into user(username,password) values('" + username + "','" + password + "')";
                 con.query(sql, function (err, result) {
                     if (err) throw err;
@@ -95,7 +95,7 @@ app.post('/login', urlencodedParser, function (req, res) {
                     console.log(result);
                 });
                 console.log("用户注册成功!");
-                var sql3 = "select id from user where username ='"+username+"'";
+                var sql3 = "select id from user where username ='" + username + "'";
                 con.query(sql3, function (err, result) {
                     if (err) throw err;
                     console.log(result);
@@ -107,58 +107,58 @@ app.post('/login', urlencodedParser, function (req, res) {
                             console.log("mailbox-Data inserted");
                         });
                     });
-                    
+
                 });
                 res.sendFile(__dirname + "/public/" + "login.html");
-            }else{
+            } else {
                 console.log("用户名已存在");
                 res.sendFile(__dirname + "/public/" + "register.html");
             }
-        }); 
-       
+        });
+
     }, "mail");
 
 })
 
-app.post("/send", urlencodedParser,function (req, res) {
+app.post("/send", urlencodedParser, function (req, res) {
     basic(function (con) {
         var receiver = req.body.receiver;
         var title = req.body.title;
         var content = req.body.content;
         var sender = currentUser;
-        var id1=11;
-        var receiver1="11";
-        var id2=22;
+        var id1 = 11;
+        var receiver1 = "11";
+        var id2 = 22;
         var sql = "insert into mail(title,content,sender,receiver) values('" + title + "','" + content + "','" + sender + "','" + receiver + "')";
         con.query(sql, function (err, result) {
             if (err) throw err;
             console.log("mail sended");
             console.log(currentUser);
-            var sql2="select id from mail where receiver ='"+receiver+"'";
+            var sql2 = "select id from mail where receiver ='" + receiver + "'";
             con.query(sql2, function (err, result) {
                 if (err) throw err;
                 result.forEach(function (item) {
-                    id1=item.id;
+                    id1 = item.id;
                 });
-                var sql3="select id from mailbox where address ='"+receiver+"'";
+                var sql3 = "select id from mailbox where address ='" + receiver + "'";
                 con.query(sql3, function (err, result) {
                     console.log(result);
                     if (err) throw err;
-                    if(result.length === 0){
+                    if (result.length === 0) {
                         console.log("目标邮箱不存在！");
-                    }else{
+                    } else {
                         result.forEach(function (item) {
                             console.log(item.id);
-                            id2=item.id;
+                            id2 = item.id;
                             console.log(id2);
-                            var sql4="insert into mail_mailbox(mail,mailbox) values("+id1+","+id2+")";
+                            var sql4 = "insert into mail_mailbox(mail,mailbox) values(" + id1 + "," + id2 + ")";
                             con.query(sql4, function (err, result) {
                                 if (err) throw err;
                                 console.log("mail_mailbox updated");
                             });
                         });
                     }
-                   
+
 
                 });
             });
@@ -168,7 +168,7 @@ app.post("/send", urlencodedParser,function (req, res) {
     res.sendFile(__dirname + "/public/" + "main.html");
 })
 app.get('/quit', urlencodedParser, function (req, res) {
-    console.log("user-"+currentUser+"-quit");
+    console.log("user-" + currentUser + "-quit");
     res.sendFile(__dirname + "/public/" + "login.html");
 })
 app.listen(3000);
