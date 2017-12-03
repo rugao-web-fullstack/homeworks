7598eaa8161e68aa44624ba32c58f5ea9f1d3c0f
wang <1447516660@qq.com>
2017-12-03 21:41:08 +0800

    格式修改

--- wd/2017-11-15/tcp.js
+++ wd/2017-11-15/tcp.js
@@ -1,8 +1,9 @@
-const net = require("net");
-const server = net.createServer((socket) = > {
-        socket.end("Hello from tcp server!\n");
-})
+const net = require('net');
+var debug = require('debug')('xxx');
+const server = net.createServer((socket) => {
+  socket.end('Hello from tcp server!\n');
+});
 let port = process.env.NODE_PORT || 8080;
-server.listen(port, () = > {
-    console.log("Server started at:" + port);
+server.listen(port, () => {
+  debug('log:' + 'Server started at:' + port);
 });
