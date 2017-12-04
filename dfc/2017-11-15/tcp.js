b4834fcf35840ef4fb5d5e0e01d6377d5c0e0a55
unknown <309425239@qq.com>
2017-12-04 09:18:09 +0800

    党方超代码规范修改

--- dfc/2017-11-15/tcp.js
+++ dfc/2017-11-15/tcp.js
@@ -1,20 +1,19 @@
-const net = require("net");
+const net = require('net');
+var debug = require('debug')('log');
 const server = net.createServer((socket) => {
-	socket.write("请输入你想输入的内容\n");
-	socket.on('data', (data) => {
-		let Data = data.toString();
-		console.log(Data);		
-		let closeSocket = Buffer.from('closeSocket\r\n');
-		let closeServer = Buffer.from('closeServer\r\n');
-		if(Buffer.compare(data, closeSocket)===0){
-			socket.end();
-		}
-		if(Buffer.compare(data, closeServer)===0){
-			socket.end();
-			server.close();
-		}
-	})
+  socket.write('请输入你想输入的内容\n');
+  socket.on('data', (data) => {
+    let closeSocket = Buffer.from('closeSocket\r\n');
+    let closeServer = Buffer.from('closeServer\r\n');
+    if(Buffer.compare(data, closeSocket)===0){
+      socket.end();
+    }
+    if(Buffer.compare(data, closeServer)===0){
+      socket.end();
+      server.close();
+    }
+  });
 });
 server.listen(process.env.NODE_POST || 8080, () => {
-	console.log('TCP开启');
+  debug('log:' + 'TCP开启');
 });
