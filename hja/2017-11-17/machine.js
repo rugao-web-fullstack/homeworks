8e6f10d0cb1d78867ef479f59d291e43e67c18f5
Snow <1011341509@qq.com>
2017-12-04 11:23:25 +0800

    风格修改

--- hja/2017-11-17/machine.js
+++ hja/2017-11-17/machine.js
@@ -1,28 +1,30 @@
+var debug = require('debug')('ma');
 let states = {
-    USER_NOT_LOGIN: 'user-not-login'
-}
+  USER_NOT_LOGIN: 'user-not-login'
+};
 
 function Machine() {
 
-    this.state = states.USER_NOT_LOGIN;
-    this.action = '';
+  this.state = states.USER_NOT_LOGIN;
+  this.action = '';
 
 }
 
 Machine.prototype.process = function (socket, data) {
-    let input = this.getCleanedString(data);
-    switch (this.state) {
-        case states.USER_NOT_LOGIN:
-        default:
-            socket.emit(states.USER_NOT_LOGIN, this, socket, data)
-    }
-}
+  let input = this.getCleanedString(data);
+  debug(input);
+  switch (this.state) {
+  case states.USER_NOT_LOGIN:
+  default:
+    socket.emit(states.USER_NOT_LOGIN, this, socket, data);
+  }
+};
 
 Machine.prototype.getCleanedString = function (data) {
-    let input = String(data);
-    input = input.replace(/\n|\r/, '');
-    return input;
+  let input = String(data);
+  input = input.replace(/\r\n/, '');
+  return input;
 
-}
+};
 
 exports.Machine = Machine;
