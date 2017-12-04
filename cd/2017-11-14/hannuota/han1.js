e34b204a3a453ef2f0680634d85a3a93e39b04a4
Eastwood <31194686+Eastwood95@users.noreply.github.com>
2017-12-04 10:08:56 +0800

    代码格式修改

--- cd/2017-11-14/hannuota/han1.js
+++ cd/2017-11-14/hannuota/han1.js
@@ -1,11 +1,12 @@
+var debug = require('debug')('xxx');
 function hanoi(n,a,b,c){
-    if(n === 1){
-        console.log("Move " + n + " from " + a + " to " + c);
-    }
-    else{
-        hanoi(n-1,a,c,b);
-        console.log("Move " + n + " from " + a + " to " + c);
-        hanoi(n-1,b,a,c);
-    }
+  if(n === 1){
+    debug('log:'+'Move ' + n + ' from ' + a + ' to ' + c);
+  }
+  else{
+    hanoi(n-1,a,c,b);
+    debug('log:'+'Move ' + n + ' from ' + a + ' to ' + c);
+    hanoi(n-1,b,a,c);
+  }
 }
 exports.hano = hanoi;
--- cd/2017-11-16/user.js
+++ cd/2017-11-16/user.js
@@ -1,12 +1,13 @@
+var debug = require('debug')('xxx');
 function User(event) {
-	this.event = event;
-	console.log("user: constructor");
+  this.event = event;
+  debug('log:' +'user: constructor');
 }
 User.prototype.register = function(username, password, email) {
-	console.log("user: register");
-	this.username = username;
-	this.password = password;
-	this.email = email;
-	this.event.emit("user-register", this);
+  debug('log:' +'user: register');
+  this.username = username;
+  this.password = password;
+  this.email = email;
+  this.event.emit('user-register', this);
 };
 exports.User = User;
