4ccb1a61e5934321349d10b9ccc6912d8535c1eb
qt19960311 <yue666-qian@example.com>
2017-12-04 13:37:47 +0800

    代码风格修改

--- qt/2017-11-14/fibonacci/fibonacci.js
+++ qt/2017-11-14/fibonacci/fibonacci.js
@@ -1,14 +1,14 @@
 function fibonacci(num) {
-    var arr = [];
-    var first = 0;
-    var second = 1;
-    var sum = 1;
-    for (var i = 0; i < num; i ++) {
-        arr.push(sum);
-        sum = first + second;
-        first = second;
-        second = sum;
-    }
-    return arr;
+  var arr = [];
+  var first = 0;
+  var second = 1;
+  var sum = 1;
+  for (var i = 0; i < num; i++) {
+    arr.push(sum);
+    sum = first + second;
+    first = second;
+    second = sum;
+  }
+  return arr;
 }
 module.exports = fibonacci;
--- qt/2017-11-29/query.js
+++ qt/2017-11-29/query.js
@@ -1,6 +1,7 @@
-var express = require('express')
-var app = express()
-app.use('/', function(req, res) {
-    console.log(req.query);
+var debug = require('debug')('log');
+var express = require('express');
+var app = express();
+app.use('/', function(req) {
+  debug('log:' + req.query);
 });
-app.listen(3000)
+app.listen(3000);
