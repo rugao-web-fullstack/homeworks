8883577510896f3af4d1cd6a603e9506b9fced98
songjieyu <sjyjsnt@163.com>
2017-12-04 10:11:49 +0800

    代码风格

--- sjy/2017-11-29/express/pug.js
+++ sjy/2017-11-29/express/pug.js
@@ -1,9 +1,8 @@
-var express = require('express')
-var pug = require('pug')
-var app = express()
+var express = require('express');
+var app = express();
 app.set('views', './templates');
 app.set('view engine', 'pug');
 app.get('/', function (req, res) {
-  res.render('index', { name: "Eric" })
-})
-app.listen(3000)
+  res.render('index', { name: 'Eric' });
+});
+app.listen(3000);
