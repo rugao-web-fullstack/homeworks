c72ef7583ce95492eb4dfaf3b20b630b13bb94d9
bbb <178200328@qq.com>
2017-12-03 23:28:53 -0800

    代码格式

--- gj/2017-11-29/classdemo/mid2.js
+++ gj/2017-11-29/classdemo/mid2.js
@@ -1,5 +1,7 @@
+var debug = require('debug')('log');
 module.exports = function (req, res, next) {
-	console.log("inside middle ware");
-	req.info = req.mid + "top";
-	next();
-}
+  // console.log('inside middle ware');
+  debug('log : ' + 'inside middle ware');
+  req.info = req.mid + 'top';
+  next();
+};
