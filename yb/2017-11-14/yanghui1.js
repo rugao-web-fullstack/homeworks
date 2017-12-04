d8f236975900bc8bcdfcf2bbaee12de7a313cc2d
yuanben1995 <974456957@qq.com>
2017-12-04 14:57:28 +0800

    规范代码

--- yb/2017-11-14/yanghui1.js
+++ yb/2017-11-14/yanghui1.js
@@ -1,11 +1,15 @@
-const yanghui = require('./yanghui');
-const readline = require('readline');// 引入readline模块
+var yanghui = require('./yanghui-1');
+var readline = require('readline');
+var debug = require('debug')('xxx');
 
-var  rl = readline.createInterface({//创建readline接口实例
-    input:process.stdin,
-    output:process.stdout
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-rl.question("杨辉三角的高度是:",function(n) {// question方法
-    console.log(yanghui(n));
-})
+rl.question('请输入数列长度：', function (n) {
+  var arr = yanghui(n);
+  for(var i = 0; i < n; i++){
+    debug('log:' + arr[i]);
+  }
+});
