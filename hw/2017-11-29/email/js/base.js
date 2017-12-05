48c67184a96af3d3cef3f0caf63885cd4720a36f
hw <1045764514@qq.com>
2017-12-04 19:27:30 -0800

    hw

--- hw/2017-11-29/email/js/base.js
+++ hw/2017-11-29/email/js/base.js
@@ -1,21 +1,22 @@
+var debug = require('debug')('xxx');
 var mysql = require('mysql');
-var init = function(cb, db) {
+var init = function (cb, db) {
 	var options = {
 		host: process.env.MYSQL_HOST,
 		user: process.env.MYSQL_USER,
 		password: process.env.MYSQL_PASSWORD
 	};
-	if(db) {
+	if (db) {
 		options.database = db;
 	}
 	var con = mysql.createConnection(options);
 	con.connect(function (err) {
-		if(err) throw err;
-		console.log('connected!');
+		if (err) throw err;
+		debug('log:' + ' connected!');
 		cb instanceof Function && cb(con);
 	});
 }
-if(!module.parent) {
+if (!module.parent) {
 	init();
 }
 module.exports = init;
--- hw/2017-11-29/email/js/login.js
+++ hw/2017-11-29/email/js/login.js
@@ -1,26 +1,27 @@
+var debug = require('debug')('xxx');
 var base = require('./base');
-module.exports = function(req, res) {
-	var user = req.body.user;
-	var password = req.body.password;
-	base(function (con) {
-		var sql = "select * from user where user = '" + user +"'";
-		con.query(sql, function(err, result) {
-			if(err) throw err;
-			if(result.length === 0) {
-				console.log('对不起，该用户名不存在！');
-				res.redirect('/');
-			} else {
-				result.forEach(function (item){
-					if (item.password !== password){
-						console.log("密码不正确！");
-						res.redirect('/');
-					} else {
-						res.cookie('name', username);
-						console.log("user "+username+" login in");
-						res.redirect('./mail');
-					}
-				});
-			}
-		});
-	}, 'mydb');
-}
+module.exports = function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  base(function (con) {
+    var sql = 'select * from user where user = \'' + username + '\'';
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      if (result.length === 0) {
+        debug('log:' + '对不起，该用户不存在！');
+        res.redirect('/');
+      } else {
+        result.forEach(function (item) {
+          if (item.password !== password) {
+            debug('log:' + '密码不正确！');
+            res.redirect('/');
+          } else {
+            res.cookie('name', username);
+            debug('log:' + 'user ' + username + ' login in');
+            res.redirect('./mail');
+          }
+        });
+      }
+    });
+  }, 'mydb');
+};
