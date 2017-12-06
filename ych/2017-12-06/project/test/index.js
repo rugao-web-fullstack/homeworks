1180ec6aebdfc97702950373bdb53be44cd22a23
SummerYch <296635947@qq.com>
2017-12-06 15:27:57 +0800

    fbi第三次提交

--- ych/2017-12-06/project/test/index.js
+++ ych/2017-12-06/project/test/index.js
@@ -2,17 +2,17 @@ const assert = require('assert');
 const hello = require('../src/').hello;
 const fib = require('../src/').fib;
 describe('project', function() {
-		it('has hello', function() {
-			console.log(hello);
-			assert.equal('Hello World', hello);
-		});
-		it('test fib 1', function() {
-			assert.equal(1, fib(0));
-			assert.equal(1, fib(1));
-			assert.equal(2, fib(2));
-			assert.equal(3, fib(3));
-			assert.equal(5, fib(4));
-			assert.equal(8, fib(5));
-			assert.equal(13, fib(6));
-		});
+  it('has hello', function() {
+    console.log(hello);
+    assert.equal('Hello World', hello);
+  });
+  it('test fib 1', function() {
+    assert.equal(1, fib(0));
+    assert.equal(1, fib(1));
+    assert.equal(2, fib(2));
+    assert.equal(3, fib(3));
+    assert.equal(5, fib(4));
+    assert.equal(8, fib(5));
+    assert.equal(13, fib(6));
+  });
 });
