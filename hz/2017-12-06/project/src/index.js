b4bc9d3b7dcefc12daab9d6c9c7fe8afd8412106
hongzheng1 <1304592019@qq.com>
2017-12-06 20:22:54 +0800

    12.6

--- hz/2017-12-06/project/src/index.js
+++ hz/2017-12-06/project/src/index.js
@@ -1,5 +1,4 @@
 exports.hello = 'Hello World';
-exports.hello = 'Hello World';
 var fib = function(n) {
   switch(n) {
   case 0:
@@ -8,12 +7,10 @@ var fib = function(n) {
   case '1':
     return 1;
   default:
-    if (n > 1) {
-      return fib(n-2) + fib(n-1);
-    }
     if (n < 0) {
       throw new Error ('error input');
     }
+    return fib(n-2) + fib(n-1);
   }
 };
 exports.fib = fib;
