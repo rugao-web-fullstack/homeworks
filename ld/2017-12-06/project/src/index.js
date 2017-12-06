7ae3b0dcac2098fdd5d2356c01c87394abd56e2a
PandaMino <1372438884@qq.com>
2017-12-06 14:38:12 +0800

    modified

--- ld/2017-12-06/project/src/index.js
+++ ld/2017-12-06/project/src/index.js
@@ -1,18 +1,15 @@
-
-var fib = function(n){
-	switch(n) {
-		case 0:
-		case '0':
-		case 1:
-		case '1':
-			return 1;
-		default:
-			if(n>1){
-				return fib(n-1) + fib(n-2);
-			}
-			if(n<0){
-				throw new Error("Error");
-			}
-	}
+var fib = function (n) {
+    switch (n) {
+    case 0:
+    case 1:
+        return 1;
+    default:
+        if (n > 1) {
+            return fib(n - 1) + fib(n - 2);
+        }
+        if (n < 0) {
+            throw new Error('error input');
+        }
+    }
 };
 exports.fib = fib;
