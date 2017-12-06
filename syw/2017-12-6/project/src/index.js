834c04e1ff1c990bde48900d4031b6fd5dd94d67
sywlove <2577472346@qq.com>
2017-12-06 19:11:42 +0800

    fbnac覆盖率

--- syw/2017-12-6/project/src/index.js
+++ syw/2017-12-6/project/src/index.js
@@ -1,16 +1,17 @@
-exports.hello = 'Hello World';
-var fib = function(n) {
-  switch(n) {
-  case 0:
-  case 1:
-    return 1;
-  default: 
-    if (n > 1) {
-      return fib(n - 2) + fib(n - 1);
-    }
-    if (n < 0) {
-      throw new Error('Error Input');
-    }
+exports.hello = "Hello World";
+var fib = function (n) {
+  switch (n) {
+    case 0:
+    case 1:
+      return 1;
+    default:
+      console.log(n);
+      if (n < 0) {
+        throw new Error("Error Input");
+      } else {
+        return fib(n - 2) + fib(n - 1);
+      }
   }
 };
-exports.fib = fib;  
+exports.fib = fib;
+
--- syw/2017-12-6/project/test/index.js
+++ syw/2017-12-6/project/test/index.js
@@ -1,15 +1,17 @@
 var assert = require('assert');
 var hello = require('../src/').hello;
 var fib = require('../src/').fib;
-var debug = require('debug')('log');
-describe('project', function() {
-  it('has hello', function() {
-    //console.log(hello);
-    debug('log:'+'heoolw');
-    assert.equal('Hello World', hello);
-  });
+var debug = require('debug')('xxx');
 
-  it('test fib 1', function() {
+describe('project', function () {
+  it('has hello', function () {
+    debug('log:' + hello);
+    assert.equal("Hello World", hello);
+
+
+
+  });
+  it('test fib 1', function () {
     assert.equal(1, fib(0));
     assert.equal(1, fib(1));
     assert.equal(2, fib(2));
@@ -22,4 +24,13 @@ describe('project', function() {
     assert.equal(55, fib(9));
     assert.equal(89, fib(10));
   });
+  it('test fib exception', function () {
+    try {
+      fib(-1);
+    }
+    catch (e) {
+      assert.equal("Error Input", e.message);
+    }
+  });
 });
+
