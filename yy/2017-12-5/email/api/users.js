f15f05ac0935153a9f1583c9590c57fdcaa56182
yangyan1012775 <yangyan1012775@163.com>
2017-12-06 10:02:00 +0800

    yy

--- yy/2017-12-5/email/api/users.js
+++ yy/2017-12-5/email/api/users.js
@@ -3,52 +3,48 @@ var router = express.Router();
 var base = require('../emaildb/base');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-    // next();
+router.get('/', function(req, res) {
+  res.send('respond with a resource');
+  // next();
 });
-router.post('/register', function(req, res, next) {
-    res.send('respond');
-    var username = req.body.username;
-    var email = req.body.email;
-    var password = req.body.password;
-    // 先查询user表中的该用户名是否被占用
-    base(function (con) {
-        var sql = "select * from user  where username='" + username + "';";
-        con.query(sql, function (err, result) {
+router.post('/register', function(req, res) {
+  res.send('respond');
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  // 先查询user表中的该用户名是否被占用
+  base(function (con) {
+    var sql = 'select * from user  where username=\'' + username + '\';';
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      if (result[0]) {
+        req.session.register=0;
+        res.redirect('/user');
+
+      } else {
+        //当用户名不存在，才执行插入user
+        base(function (con) {
+          var sql = 'INSERT INTO user (username,password) VALUES(\'' + username + '\',\'' + password + '\');';
+          con.query(sql, function (err, result) {
             if (err) throw err;
-            if (result[0]) {
-                req.session.register=0;
+            // 将该用户的id 与email一起插入进email表中
+            var userId=result.insertId;
+            base(function (con) {
+              var sql = 'INSERT INTO emailBox (user,address) VALUES(\'' + userId + '\',\'' + email + '\');';
+              con.query(sql, function (err) {
+                if (err) throw err;
+                req.session.register=1;
                 res.redirect('/user');
 
-            } else {
-                //当用户名不存在，才执行插入user
-                base(function (con) {
-                    var sql = "INSERT INTO user (username,password) VALUES('" + username + "','" + password + "');";
-                    con.query(sql, function (err, result) {
-                        if (err) throw err;
-                        console.log('1 user record inserted!');
-                        console.log(result);
-                        // 将该用户的id 与email一起插入进email表中
-                        var userId=result.insertId;
-                        base(function (con) {
-                            var sql = "INSERT INTO emailBox (user,address) VALUES('" + userId + "','" + email + "');";
-                            con.query(sql, function (err, result) {
-                                if (err) throw err;
-                                console.log('1 emailBox record inserted!');
-                                console.log(result);
-                                req.session.register=1;
-                                res.redirect('/user');
+              });
+            }, 'emaildb');
+          });
+        }, 'emaildb');
+      }
+    });
+  }, 'emaildb');
 
-                            });
-                        }, 'emaildb');
-                    });
-                }, 'emaildb');
-            }
-        });
-    }, 'emaildb');
-
-    // next();
+  // next();
 });
 
 module.exports = router;
--- yy/2017-12-5/email/routes/user.js
+++ yy/2017-12-5/email/routes/user.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
   // next();
 });
-router.get('/register', function(req, res, next) {
-    res.render('register',{
-      title:'注册'
-    });
-    // next();
+router.get('/register', function(req, res) {
+  res.render('register',{
+    title:'注册'
+  });
+  // next();
 });
 
 module.exports = router;
