7dc4daad46deba0ad828751e50aec1605d01fd57
sywlove <2577472346@qq.com>
2017-12-06 20:44:29 +0800

    fbnc3

--- syw/2017-12-6/project/src/index.js
+++ syw/2017-12-6/project/src/index.js
@@ -1,17 +1,17 @@
-exports.hello = 'Hello world';
+exports.hello = 'Hello World';
 var fib = function (n) {
   switch (n) {
   case 0:
+  case '0':
   case 1:
-      return 1;
-  default:
-    console.log(n);
+  case '1':
+    return 1;
+  default:  
     if (n < 0) {
-        throw new Error('Error Input');
-      }
-      return fib(n - 2) + fib(n - 1);
-
+      throw new Error('Error Input');
+    }
+    return fib(n - 2) + fib(n - 1);
   }
 };
-exports.fib = fib;
 
+exports.fib = fib;
