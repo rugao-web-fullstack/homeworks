b68f41f9b5e59452bcd4f5d847ac3a5ed94c4c63
panda2451 <767181742@qq.com>
2017-12-06 20:29:27 +0800

    feibonaci代码覆盖

--- cyb/2017-12-06/project/src/index.js
+++ cyb/2017-12-06/project/src/index.js
@@ -1,15 +1,15 @@
-exports.hello = "Hello World";
+exports.hello = 'Hello World';
 var fib = function (n) {
-	switch (n) {
-		case 0:
-		case 1:
-			return 1;
-		default:
-			if (n < 0) {
-				throw new Error("Error Input");
-			}
-			return fib(n - 2) + fib(n - 1);
-	}
+  switch (n) {
+  case 0:
+  case 1:
+    return 1;
+  default:
+    if (n < 0) {
+      throw new Error('Error Input');
+    }
+    return fib(n - 2) + fib(n - 1);
+  }
 };
 
 exports.fib = fib;
