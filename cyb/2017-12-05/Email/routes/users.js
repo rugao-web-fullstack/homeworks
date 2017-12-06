c79f1f554e8e9cc1ecea7cecacd478312c74afaa
yangyan1012775 <yangyan1012775@163.com>
2017-12-06 10:22:02 +0800

    yy

--- cyb/2017-12-05/Email/routes/users.js
+++ cyb/2017-12-05/Email/routes/users.js
@@ -5,11 +5,11 @@ var User = require('../mov/user');
 /* GET users listing. */
 
 router.post('/login', function (req, res, next) {
-	User(req, res, next);
+  User(req, res, next);
 });
 
 router.post('/register', function (req, res, next) {
-	User(req, res, next);
+  User(req, res, next);
 });
 
 module.exports = router;
