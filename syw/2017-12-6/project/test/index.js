f06e3e8e2ed865a2302e10e7c10d96b3490640f8
sywlove <2577472346@qq.com>
2017-12-06 20:07:08 +0800

    fbnc1

--- syw/2017-12-6/project/test/index.js
+++ syw/2017-12-6/project/test/index.js
@@ -1,23 +1,28 @@
 var assert = require('assert');
 var hello = require('../src/').hello;
-var fib = require('../src/').fib;
+var fib=require('../src/').fib;
 describe('project', function () {
   it('has hello', function () {
-    assert.equal('Hello world', hello);
+    // console.log(hello);
+    assert.equal('Hello World', hello);
   });
-  it('test fib', function () {
-    assert.equal(1, fib(0));
-    assert.equal(1, fib(1));
-    assert.equal(2, fib(2));
-    assert.equal(3, fib(3));
-    assert.equal(5, fib(4));
-    assert.equal(89, fib(10));
+
+  it('test fib 1',function(){
+    assert.equal(1,fib(0));
+    assert.equal(1,fib(1));
+    assert.equal(2,fib(2));
+    assert.equal(3,fib(3));
+    assert.equal(5,fib(4));
+    assert.equal(89,fib(10));
   });
+
   it('test fib exception', function(){
     try{
-      fib(-1);  
-    }catch(e){
-      assert.equal('Error Input', e.message);   
+      fib(-1);
+    }catch(e) {
+      assert.equal('Error Input',e.message);
     }
+
   });
+    
 });
