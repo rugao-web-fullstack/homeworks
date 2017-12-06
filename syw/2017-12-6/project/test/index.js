0792d8d74d63946ad3098f10af1fcb4621548469
sywlove <2577472346@qq.com>
2017-12-06 19:25:05 +0800

    fbnc覆盖率

--- syw/2017-12-6/project/test/index.js
+++ syw/2017-12-6/project/test/index.js
@@ -1,36 +1,23 @@
 var assert = require('assert');
 var hello = require('../src/').hello;
 var fib = require('../src/').fib;
-var debug = require('debug')('xxx');
-
 describe('project', function () {
   it('has hello', function () {
-    debug('log:' + hello);
-    assert.equal("Hello World", hello);
-
-
-
+    assert.equal('Hello world', hello);
   });
-  it('test fib 1', function () {
+  it('test fib', function () {
     assert.equal(1, fib(0));
     assert.equal(1, fib(1));
     assert.equal(2, fib(2));
     assert.equal(3, fib(3));
     assert.equal(5, fib(4));
-    assert.equal(8, fib(5));
-    assert.equal(13, fib(6));
-    assert.equal(21, fib(7));
-    assert.equal(34, fib(8));
-    assert.equal(55, fib(9));
     assert.equal(89, fib(10));
   });
-  it('test fib exception', function () {
-    try {
-      fib(-1);
-    }
-    catch (e) {
-      assert.equal("Error Input", e.message);
+  it('test fib exception', function(){
+    try{
+      fib(-1);  
+    }catch(e){
+      assert.equal('Error Input', e.message);   
     }
   });
 });
-
