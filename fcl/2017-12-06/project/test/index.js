0bc3798db3362cc39ec33378ee5a1587e59996b0
dlyfcl <fcldly@163.com>
2017-12-07 11:53:58 +0800

    fbnq

---
+++ fcl/2017-12-06/project/test/index.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var hello = require('../src/').hello;
+var fib = require('../src/').fib;
+describe('ceshi', function() {
+    it('has hello', function() {
+        console.log(hello);
+        assert.equal("Hello World", hello)
+    });
+
+    it('test fib 1', function() {
+        assert.equal(1, fib(0));
+        assert.equal(1, fib(1));
+        assert.equal(2, fib(2));
+        assert.equal(3, fib(3));
+    });
+
+    it('test fib exception', function() {
+        try {
+            fib(-1);
+        } catch (e) {
+            assert.equal("Error Input", e.message);
+        }
+    })
+});
