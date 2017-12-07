8350a641f21332b1993fcd4f51da27d91dc87fbc
pan <770258173@qq.com>
2017-12-07 11:37:22 +0800

    hanoi

---
+++ pqq/2017-12-06/hanoi/test/index.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var hanio = require('../src/hanio');
+describe('project', function () {
+  it('test hanio', function () {
+    assert.equal('from a to c', hanio(1));
+  });
+  it('test hanio2', function () {
+    assert.equal('from a to b,from a to c,from b to c', hanio(2));
+  });
+  it('test hanio exception', function () {
+    try {
+      hanio(0);
+    } catch (e) {
+      assert.equal('Error Input', e.message);
+    }
+  });
+});
--- pqq/2017-12-06/project/test/index.js
+++ pqq/2017-12-06/project/test/index.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var hello = require('../src/').hello;
 var fib = require('../src/index').fib;
-var hanio = require('../src/hanio');
+
 
 
 describe('project', function () {
@@ -32,20 +32,4 @@ describe('project', function () {
     }
   });
 
-});
-
-describe('project', function () {
-  it('test hanio', function () {
-    assert.equal('from a to c', hanio(1));
-  });
-  it('test hanio2', function () {
-    assert.equal('from a to b,from a to c,from b to c', hanio(2));
-  });
-  it('test hanio exception', function () {
-    try {
-      hanio(0);
-    } catch (e) {
-      assert.equal('Error Input', e.message);
-    }
-  });
 });
