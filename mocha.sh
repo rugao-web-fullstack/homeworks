a0198d7bc4fc86d9ab651b0c24eb7d96149ee479
calidion <calidion@gmail.com>
2017-12-07 14:09:26 +0800

    enabled session

--- mocha.sh
+++ mocha.sh
@@ -1,3 +1,5 @@
+CMD="./node_modules/nyc/bin/nyc.js --check-coverage --lines 100 --functions 95 --branches 95 ./node_modules/.bin/mocha"
+
 for file in *
 do
   if [ "$file" == "node_modules" ];then
@@ -7,11 +9,16 @@ do
   if [ -d "$file" ];then
     if [ -d "$file/2017-12-06/project" ]; then
       echo "inside mocha $file ..."
-      ./node_modules/nyc/bin/nyc.js --check-coverage --lines 100 --functions 95 --branches 95 ./node_modules/.bin/mocha "$file/2017-12-06/project/test" || exit 1;
+      $CMD "$file/2017-12-06/project/test" || exit 1;
     fi
     if [ -d "$file/2017-12-06/hanoi" ]; then
       echo "inside hanoi coverage $file ..."
-      ./node_modules/nyc/bin/nyc.js --check-coverage --lines 100 --functions 95 --branches 95 ./node_modules/.bin/mocha "$file/2017-12-06/hanoi/test" || exit 1;
+      $CMD "$file/2017-12-06/hanoi/test" || exit 1;
     fi
+    if [ -d "$file/2017-12-07/session" ]; then
+      echo "inside session coverage $file ..."
+      $CMD "$file/2017-12-07/session/test" || exit 1;
+    fi
+
   fi
 done
