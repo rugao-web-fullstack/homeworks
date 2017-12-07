9a9c514fa1b748e4d34edeac67874873e12a18d5
Ccroo <Ccrool@163.com>
2017-12-07 08:43:36 +0800

    覆盖率

--- hja/2017-12-06/project/test/index.js
+++ hja/2017-12-06/project/test/index.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
 
 var hello = require('../src/').hello;
-var fib = require('../src/').fib;
+var fib = require('../src/index').fib;
+var han = require('../src/index2');
 
 describe('project', function () {
 
@@ -26,3 +27,16 @@ describe('project', function () {
   });
 
 });
+
+
+describe('This is hannuo test', function () {
+  it('true hannuo', function () {
+    assert.equal('1,A,C', han.han1(1, 'A', 'B', 'C'));
+    assert.equal('1,A,B,2,A,C,1,B,C', han.han1(2, 'A', 'B', 'C'));
+    assert.equal('1,A,C,2,A,B,1,C,B,3,A,C,1,B,A,2,B,C,1,A,C', han.han1(3, 'A', 'B', 'C'));
+  });
+
+});
+
+
+
