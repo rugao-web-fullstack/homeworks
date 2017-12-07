a7c2de695054f6bfa123157752cc398f9b7aefdc
sywlove <2577472346@qq.com>
2017-12-07 12:18:19 +0800

    hanoi3

--- syw/2017-12-06/hanoi/test/index.js
+++ syw/2017-12-06/hanoi/test/index.js
@@ -1,14 +1,14 @@
 var assert = require('assert');
-var hunt = require('../src/hnt.js');
-
+var hanoi = require('../src/hanoi');
 describe('project', function () {
-  it('test hnt', function () {
-    assert.equal('from a to c', hunt(1));
-    assert.equal('from a to b,from a to c,from b to c', hunt(2));
+  it('test hanoi', function () {
+    assert.equal('from a to c', hanoi(1));
+    assert.equal('from a to b,from a to c,from b to c', hanoi(2));
   });
-  it('test hnt exception', function () {
+  
+  it('test hanoi exception', function () {
     try {
-      hunt(0);
+      hanoi(0);
     } catch (e) {
       assert.equal('Error Input', e.message);
     }
