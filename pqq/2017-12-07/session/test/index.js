30027f8b2029446a2a016e60b15aecbd4baacf94
pan <770258173@qq.com>
2017-12-07 14:28:17 +0800

    session

---
+++ pqq/2017-12-07/session/test/index.js
@@ -0,0 +1,31 @@
+var request = require('supertest');
+var assert = require('assert');
+var app = require('../src/app').app;
+var cookies;
+
+describe('POST /user/login', function () {
+  it('should respond with json', function (done) {
+    request(app)
+      .get('/')
+      .expect(200, function (err, res) {
+        // console.log(res.headers);
+        cookies = res.headers['set-cookie'];
+
+        assert(cookies !== null);
+        assert(res.body, 1);
+        done();
+      });
+  });
+
+  it('should respond with json', function (done) {
+    var req = request(app)
+      .get('/');
+    req.cookies = cookies;
+    req
+      .expect(200, function (err, res) {
+        assert(res.body, 2);
+        done();
+      });
+  });
+
+});
