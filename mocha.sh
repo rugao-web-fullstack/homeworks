db2489f99a6ef0740e25a20bb48770605b9918e7
calidion <calidion@gmail.com>
2017-12-07 11:21:36 +0800

    fixed eslint problem

--- mocha.sh
+++ mocha.sh
@@ -9,5 +9,9 @@ do
       echo "inside mocha $file ..."
       ./node_modules/nyc/bin/nyc.js --check-coverage --lines 100 --functions 95 --branches 95 ./node_modules/.bin/mocha "$file/2017-12-06/project/test" || exit 1;
     fi
+    if [ -d "$file/2017-12-06/hanoi" ]; then
+      echo "inside hanoi coverage $file ..."
+      ./node_modules/nyc/bin/nyc.js --check-coverage --lines 100 --functions 95 --branches 95 ./node_modules/.bin/mocha "$file/2017-12-06/project/hanoi" || exit 1;
+    fi
   fi
 done
