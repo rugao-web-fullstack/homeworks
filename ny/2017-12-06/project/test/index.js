fe54c07cacb1154e3249995d8e4e633b07c593c1
nylovelan <nylovelan@outlook.com>
2017-12-07 11:26:03 +0800

    汉诺塔

--- ny/2017-12-06/project/test/index.js
+++ ny/2017-12-06/project/test/index.js
@@ -1,7 +1,6 @@
 var assert = require('assert');
 var hello = require('../src/').hello;
 var fib = require('../src/').fib;
-var hanoi = require('../src/hanoi').hanoi;
 describe('project', function() {
   it('has hello', function() {
     assert.equal('Hello World', hello);
@@ -18,17 +17,6 @@ describe('project', function() {
     try {
       fib(-1);
     } catch(e) {
-      assert.equal('error input', e.message);
-    }
-  });
-  it('test hanoi', function () {
-    assert.equal('from a to c', hanoi(1));
-    assert.equal('from a to b,from a to c,from b to c', hanoi(2));
-  });
-  it('test hanoi exception', function () {
-    try {
-      hanoi(0);
-    } catch (e) {
       assert.equal('error input', e.message);
     }
   });
