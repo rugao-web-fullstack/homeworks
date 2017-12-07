9fb9583126dda1f5513b439160c2dbab2804dfc8
hw <1045764514@qq.com>
2017-12-06 22:42:38 -0800

    hw

--- hw/2017-12-06/project/test/index.js
+++ hw/2017-12-06/project/test/index.js
@@ -8,12 +8,20 @@ describe('Project', function () {
     assert.equal(-1, [1, 2, 3].indexOf(4));
   });
 
-  it('test fib 1', function () {
+  it('test fib', function () {
     assert.equal(1, fib(0));
     assert.equal(1, fib(1));
     assert.equal(2, fib(2));
     assert.equal(3, fib(3));
     assert.equal(5, fib(4));
     assert.equal(8, fib(5));
+  });
+
+  it('test fib exception', function () {
+    try {
+      fib(-100);
+    } catch (e) {
+      e;
+    }
   });
 });
