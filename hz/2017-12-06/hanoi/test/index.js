0c7cfc7d0ee4fe45457963010f6d025ca5ee7d8d
hongzheng1 <1304592019@qq.com>
2017-12-07 11:43:51 +0800

    12.6hanoi

---
+++ hz/2017-12-06/hanoi/test/index.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var hunt = require('../src/hnt.js');
+
+describe('project', function () {
+  it('test hnt', function () {
+    assert.equal('from a to c', hunt(1));
+  });
+  it('test hnt2', function () {
+    assert.equal('from a to b,from a to c,from b to c', hunt(2));
+  });
+  it('test hnt exception', function () {
+    try {
+      hunt(0);
+    } catch (e) {
+      assert.equal('Error Input', e.message);
+    }
+  });
+});
