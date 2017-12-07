dbbc659f99d7f03429bb02008ac3af18b699595c
calidion <calidion@gmail.com>
2017-12-07 15:46:01 +0800

    enabled mysql

--- rwx/2017-12-07/mail/test/index.js
+++ rwx/2017-12-07/mail/test/index.js
@@ -1,9 +1,27 @@
 var assert = require('assert');
+var mysql = require('../src/').mysql;
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+
+  describe('mysql()', function() {
+    it('should connect to mysql', function(done) {
+      mysql(function(con) {
+        assert(con);
+        con.end();
+        done();
+      });
+    });
+    it('should connect to mysql', function() {
+      try {
+        mysql();
+      } catch(e) {
+        assert(e);
+      }
     });
   });
 });
