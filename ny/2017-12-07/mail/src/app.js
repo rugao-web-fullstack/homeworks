9f363c798e4af13d6ac5da326149fc06fb61f7ae
nylovelan <nylovelan@outlook.com>
2017-12-08 16:48:30 +0800

    mail2

--- ny/2017-12-07/mail/src/app.js
+++ ny/2017-12-07/mail/src/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var mails = require('./routes/mails');
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -12,5 +13,6 @@ app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', index);
 app.use('/users', users);
+app.use('/mails', mails);
 // app.listen(3000);
 exports.app = app;
