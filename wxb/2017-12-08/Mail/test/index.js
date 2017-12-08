bfe49651d634bfc4f62d1a4cd0ab5755e98aadab
O-Mr-Wei-O <1223157723@qq.com>
2017-12-08 16:11:03 +0800

    eslint

--- wxb/2017-12-08/Mail/test/index.js
+++ wxb/2017-12-08/Mail/test/index.js
@@ -1,67 +1,67 @@
 var request = require('supertest');
-var assert = require('assert');
+//var assert = require('assert');
 var app = require('../src/app');
 
 describe('URL test', function () {
   it('get /', function (done) {
     request(app)
-            .get('/')
-            .expect('Content-Type', 'text/html; charset=utf-8')
-            .expect(200)
-            .end(function(err, res){
-                if (err) throw err;
-                done();
-            });
+      .get('/')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200)
+      .end(function(err){
+        if (err) throw err;
+        done();
+      });
   });
   it('get /users/register', function (done) {
     request(app)
-            .get('/users/register')
-            .expect('Content-Type', 'text/html; charset=utf-8')
-            .expect(200)
-            .end(function(err, res){
-                if (err) throw err;
-                done();
-            });
+      .get('/users/register')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200)
+      .end(function(err){
+        if (err) throw err;
+        done();
+      });
   });
   it('get /users/login', function (done) {
     request(app)
-            .get('/users/login')
-            .expect('Content-Type', 'text/html; charset=utf-8')
-            .expect(200)
-            .end(function(err, res){
-                if (err) throw err;
-                done();
-            });
+      .get('/users/login')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200)
+      .end(function(err){
+        if (err) throw err;
+        done();
+      });
   });
   it('get /users/mail', function (done) {
     request(app)
-            .get('/users/mail')
-            .expect('Content-Type', 'text/html; charset=utf-8')
-            .expect(200)
-            .end(function(err, res){
-                if (err) throw err;
-                done();
-            });
+      .get('/users/mail')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200)
+      .end(function(err){
+        if (err) throw err;
+        done();
+      });
   });
   it('get /users/mail/write', function (done) {
     request(app)
-            .get('/users/mail/write')
-            .expect('Content-Type', 'text/html; charset=utf-8')
-            .expect(200)
-            .end(function(err, res){
-                if (err) throw err;
-                done();
-            });
+      .get('/users/mail/write')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200)
+      .end(function(err){
+        if (err) throw err;
+        done();
+      });
   });
   it('get /users/mail/all', function (done) {
     request(app)
-            .get('/users/mail/all')
-            .expect('Content-Type', 'text/html; charset=utf-8')
-            .expect(200)
-            .end(function(err, res){
-                if (err) throw err;
-                done();
-            });
+      .get('/users/mail/all')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200)
+      .end(function(err){
+        if (err) throw err;
+        done();
+      });
   });
 
 });
