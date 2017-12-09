fef79b05c320455008a3cba39c89736ddb459792
pan <770258173@qq.com>
2017-12-09 11:39:42 +0800

    URL--user和mail的get测试

---
+++ pqq/2017-12-07/mail/read.md
@@ -0,0 +1,10 @@
+
+###GET
+
+##user
+- /user/main  ---首页
+- /user/register   ---注册
+- /user/login  ---登录
+
+##mail
+- /mail/main   ---邮箱 
---
+++ pqq/2017-12-07/mail/src/routes/user.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/register', function (req, res) {
+  res.render('register',{title :'register'});
+  return;
+});
+router.get('/login', function (req, res) {
+  res.render('login',{title :'login'});
+  return;
+});
+router.get('/main', function (req, res) {
+  res.render('main',{title :'main'});
+  return;
+});
+module.exports = router;
