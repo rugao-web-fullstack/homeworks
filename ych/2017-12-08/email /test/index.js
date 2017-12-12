4218c10e5680469880c2832f4a96017eddbf3bf6
SummerYch <296635947@qq.com>
2017-12-12 03:56:40 -0800

    db test

--- ych/2017-12-08/email /test/index.js
+++ ych/2017-12-08/email /test/index.js
@@ -1,7 +1,8 @@
 var request = require('supertest');
 var assert = require('assert');
 var app = require('../src/').app;
-
+var mysql = require('mysql');
+var basic = require('../src/connectServer');
 describe('url', function(){
   it('get /', function (done) {
     request(app)
@@ -26,5 +27,34 @@ describe('url', function(){
         assert((res.text).indexOf('register') !== -1);
         done();
       });
+  });
+});
+describe('db test', function(){
+  before(function (done) {
+    var con = mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD
+    });
+    con.query('drop database dmail', function () {
+      con.query('create database dmail', function () {
+        con.end();
+        done();
+      });
+    });
+  });
+  it('connect to mysql', function (done) {
+    basic(function (con) {
+      assert(con);
+      con.end();
+      done();
+    });
+  });
+  it('connect to dmail', function (done) {
+    basic(function (con) {
+      assert(con);
+      con.end();
+      done();
+    }, 'dmail');
   });
 });
