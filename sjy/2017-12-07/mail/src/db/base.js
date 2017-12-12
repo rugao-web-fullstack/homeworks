49af4be175abbdf01868a58d7d2d50e5edc7bbb0
songjieyu <sjyjsnt@163.com>
2017-12-12 04:40:41 -0800

    mail third

--- sjy/2017-12-07/mail/src/db/base.js
+++ sjy/2017-12-07/mail/src/db/base.js
@@ -1,22 +1,22 @@
 var mysql = require('mysql');
 var debug = require('debug')('xxx');
 var init = function (cb, db) {
-    var options = {
-        // host: 'localhost',
-        // user: 'root',
-        // password: '123'
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USERNAME,
-        password: process.env.MYSQL_PASSWORD
-    };
-    if (db) {
-        options.database = db;
-    }
-    var con = mysql.createConnection(options);
-    con.connect(function (err) {
-        debug('error:' + err);
-        debug('log:' + 'Connected!');
-        cb instanceof Function && cb(con);
-    });
+  var options = {
+    // host: 'localhost',
+    // user: 'root',
+    // password: '123'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD
+  };
+  if (db) {
+    options.database = db;
+  }
+  var con = mysql.createConnection(options);
+  con.connect(function (err) {
+    debug('error:' + err);
+    debug('log:' + 'Connected!');
+    cb instanceof Function && cb(con);
+  });
 };
 module.exports = init;
--- sjy/2017-12-07/mail/test/testdb.js
+++ sjy/2017-12-07/mail/test/testdb.js
@@ -1,35 +1,35 @@
-var request = require('supertest');
+// var request = require('supertest');
 var assert = require('assert');
 var mysql = require('mysql');
 var basic = require('../src/db/base');
 
 describe('数据库链接测试', function () {
-    before(function (done) {
-      // 创建数据库
-      var con = mysql.createConnection({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USERNAME,
-        password: process.env.MYSQL_PASSWORD
-      });
-      con.query('DROP DATABASE db', function () {
-        con.query('CREATE DATABASE dmail', function () {
-          con.end();
-          done();
-        });
-      });
+  before(function (done) {
+    // 创建数据库
+    var con = mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD
     });
-    it('connect to mysql', function (done) {
-      basic(function (con) {
-        assert(con);
+    con.query('DROP DATABASE db', function () {
+      con.query('CREATE DATABASE dmail', function () {
         con.end();
         done();
       });
-    });
-    it('connect to db', function (done) {
-      basic(function (con) {
-        assert(con);
-        con.end();
-        done();
-      }, 'db');
     });
   });
+  it('connect to mysql', function (done) {
+    basic(function (con) {
+      assert(con);
+      con.end();
+      done();
+    });
+  });
+  it('connect to db', function (done) {
+    basic(function (con) {
+      assert(con);
+      con.end();
+      done();
+    }, 'db');
+  });
+});
