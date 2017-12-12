1fd93c27a85ca837c59285a2c764fc831b89bca8
O-Mr-Wei-O <1223157723@qq.com>
2017-12-12 14:48:49 +0800

    remove

--- wxb/2017-12-07/mail/src/app.js
+++ wxb/2017-12-07/mail/src/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -15,10 +15,10 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
